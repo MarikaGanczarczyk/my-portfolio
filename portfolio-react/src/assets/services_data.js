4d526b1f3fa65dd743bd41e9b4080647411383fe
Marika <marikabartman27@gmail.com>
2025-08-21 11:26:17 +0100

    removed cards from services

--- portfolio-react/src/assets/services_data.js
+++ portfolio-react/src/assets/services_data.js
@@ -2,33 +2,33 @@ const Services_Data = [
     {
         s_no:"01",
         s_name:"Web design",
-        s_desc:"Web development is the process of building, programming..."
+        s_desc:"Creating intuitive, responsive, and visually engaging web designs that provide an excellent user experience on any device."
     },
     {
         s_no:"02",
-        s_name:"Graphics design",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"App design",
+        s_desc:"From wireframes to high-fidelity prototypes, I create app designs that are simple, user-friendly, and responsive across devices."
     },
     {
         s_no:"03",
-        s_name:"Social media",
-        s_desc:"Web development is the process of building, programming..."
+        s_name:"Mobile App Development",
+        s_desc:"I develop high-quality mobile apps for iOS and Android, combining smooth performance, intuitive UX, and modern design."
     },
-    {
-        s_no:"04",
-        s_name:"App design",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"05",
-        s_name:"Digital marketing",
-        s_desc:"Web development is the process of building, programming..."
-    },
-    {
-        s_no:"06",
-        s_name:"Content writing",
-        s_desc:"Web development is the process of building, programming..."
-    },
+    // {
+    //     s_no:"04",
+    //     s_name:"App design",
+    //     s_desc:"From wireframes to high-fidelity prototypes, I create app designs that are simple, user-friendly, and responsive across devices."
+    // },
+    // {
+    //     s_no:"05",
+    //     s_name:"Digital marketing",
+    //     s_desc:"Web development is the process of building, programming..."
+    // },
+    // {
+    //     s_no:"06",
+    //     s_name:"Content writing",
+    //     s_desc:"Web development is the process of building, programming..."
+    // },
 ]
  
 export default Services_Data;
