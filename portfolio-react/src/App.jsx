7a26c0347a0b1fd78ee30d1f39628aa47c49098e
Marika <marikabartman27@gmail.com>
2025-10-16 18:06:21 +0100

    Removed Services component and added new project

--- portfolio-react/src/App.jsx
+++ portfolio-react/src/App.jsx
@@ -1,29 +1,20 @@
-
 import { Routes, Route, useLocation } from "react-router-dom";
 import Navbar from "./Components/Navbar/Navbar";
 import Hero from "./Components/Hero/Hero";
 import About from "./Components/About/About";
-import Services from "./Components/Services/Services";
+
 import MyWork from "./Components/MyWork/MyWork";
 import Contact from "./Components/Contact/Contact";
 import Footer from "./Components/Footer/Footer";
 import ProjectCard from "./Components/ProjectCard/ProjectCard";
 
-
-
-
-
-
 const App = () => {
-  const location = useLocation()
+  const location = useLocation();
   const showNavbar = location.pathname === "/";
 
-
-
-
   return (
-      <div className="app">
-      {showNavbar && <Navbar  />}
+    <div className="app">
+      {showNavbar && <Navbar />}
 
       <Routes>
         <Route
@@ -32,7 +23,7 @@ const App = () => {
             <>
               <Hero />
               <About />
-              <Services />
+
               <MyWork />
               <Contact />
               <Footer />
--- portfolio-react/src/assets/mywork_data.js
+++ portfolio-react/src/assets/mywork_data.js
@@ -1,40 +1,49 @@
 import project1_img from "../assets/project_one.svg";
 import project2_img from "../assets/project_two.svg";
 import project3_img from "../assets/project_three.svg";
-import project4_img from "../assets/project_four.svg";
+import project4_img from "../assets/project_weather2.png";
 import project5_img from "../assets/project_four.svg";
 import project6_img from "../assets/project_four.svg";
 
 const mywork_data = [
-  {
+   {
     w_no: 1,
-    title: "Memory Game",
-    w_img: project1_img,
-    description: "An interactive memory matching game built with React : flip the cards, find matching pairs, and challenge your focus! Built with React (Hooks) and modern JavaScript, styled with CSS Modules, and powered by Vite / Create React App.",
-    liveLink: "https://memory-game-magic.netlify.app/",
-    githubLink: "https://github.com/MarikaGanczarczyk/memory-game.git",
+    w_name: "Weather Forecast",
+    w_img: project4_img,
+    description:
+      "Weather Live Forecast is a simple and responsive weather application built with React and CSS. It uses a public Weather API to fetch live weather data for the selected city — showing both the current weather and a 7-day forecast.",
+    liveLink: "https://weather-live-a.netlify.app/",
+    githubLink: "https://github.com/MarikaGanczarczyk/weather-app",
   },
+
   {
     w_no: 2,
     title: "NC-News",
     w_img: project2_img,
-    description: "This is the front-end application for a full-stack news platform, designed to display and interact with articles, topics, and user comments. Users can browse articles by topic, view full article content, upvote articles, and post/delete comments (if logged in).Built using React, this responsive single-page application consumes data from a RESTful API built in the back end.",
+    description:
+      "This is the front-end application for a full-stack news platform, designed to display and interact with articles, topics, and user comments. Users can browse articles by topic, view full article content, upvote articles, and post/delete comments (if logged in).Built using React, this responsive single-page application consumes data from a RESTful API built in the back end.",
     liveLink: "https://marika-ganczarczyk-nc-news.netlify.app/home",
     githubLink: "https://github.com/MarikaGanczarczyk/NC-News-FE.git",
   },
-  {
+    {
     w_no: 3,
-    title: "My Portfolio",
-    w_img: project3_img,
-    description: "This portfolio was built with React to showcase my projects and skills as a front-end developer. It features a clean and responsive design, smooth navigation, and reusable components that make it easy to expand with new projects. By using React (with hooks) and modern JavaScript, I focused on creating an interactive, fast, and user-friendly experience that reflects both my technical skills and design approach.",
-    liveLink: "https://marika-ganczarczyk.netlify.app/",
-    githubLink: "https://github.com/MarikaGanczarczyk/my-portfolio.git",
+    title: "Memory Game",
+    w_img: project1_img,
+    description:
+      "An interactive memory matching game built with React : flip the cards, find matching pairs, and challenge your focus! Built with React (Hooks) and modern JavaScript, styled with CSS Modules, and powered by Vite / Create React App.",
+    liveLink: "https://memory-game-magic.netlify.app/",
+    githubLink: "https://github.com/MarikaGanczarczyk/memory-game.git",
   },
   {
     w_no: 4,
-    w_name: "Web design",
-    w_img: project4_img,
+    title: "My Portfolio",
+    w_img: project3_img,
+    description:
+      "This portfolio was built with React to showcase my projects and skills as a front-end developer. It features a clean and responsive design, smooth navigation, and reusable components that make it easy to expand with new projects. By using React (with hooks) and modern JavaScript, I focused on creating an interactive, fast, and user-friendly experience that reflects both my technical skills and design approach.",
+    liveLink: "https://marika-ganczarczyk.netlify.app/",
+    githubLink: "https://github.com/MarikaGanczarczyk/my-portfolio.git",
   },
+ 
   {
     w_no: 5,
     w_name: "Web design",
