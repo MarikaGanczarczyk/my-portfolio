e47225e51ea9ed9468297ac313a727222f9887d4
Marika <marikabartman27@gmail.com>
2025-10-20 19:34:39 +0100

    Changed Hero.jsx file

--- portfolio-react/src/Components/Hero/Hero.jsx
+++ portfolio-react/src/Components/Hero/Hero.jsx
@@ -6,8 +6,8 @@ import AnchorLink from 'react-anchor-link-smooth-scroll';
 const Hero = () => {
   return (
     <div id='home' className='hero'>
-      <img src={MyPic} alt="" />
-      <h1><span >Hi! I’m Marika Ganczarczyk,</span> a Junior Frontend Developer passionate about building clean, responsive, and user-friendly websites. Welcome to my portfolio — here you’ll find some of my recent projects and skills. </h1>
+      {/* <img src={MyPic} alt="" /> */}
+      <h1><span >Hi! I’m Marika Ganczarczyk,</span> a Junior Full Stack Developer passionate about building clean, responsive, and user-friendly websites. Welcome to my portfolio — here you’ll find some of my recent projects and skills. </h1>
       <p>I'm not a typical developer—and that's the point. My journey from finance to tech wasn't a straight line, but every detour taught me something valuable about solving real problems. I'm comfortable being uncomfortable, whether that's learning a new programming language or understanding a complex business domain. I bring curiosity, adaptability, and a knack for translating messy challenges into clean solutions.
 </p>
       <div className="hero-action">
